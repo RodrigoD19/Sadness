ccb559423d5e25e2445cf8ea4b06f8fd19c0908f
RodrigoD19 <121464309+RodrigoD19@users.noreply.github.com>
2022-12-30 17:05:46 +0000

    Update Animais.cpp

--- Trabalhocomerro/Animais.cpp
+++ Trabalhocomerro/Animais.cpp
@@ -2,16 +2,15 @@
 // Created by rodri on 22/11/2022.
 //
 
+#include <cstring>
 #include "Animais.h"
 #include "Reserva.h"
-
-
 Animais::Animais(int posX,int posY, char letra) {
 
     posx =posX;
     posy = posY;
     sig=letra;
-  
+
     //std::cout<<a.pa();
             //rand()%(500-16 + 1) + 16;
    // t << "Criei o animal "<<Sigla<<"\n";
@@ -20,40 +19,23 @@ Animais::Animais(int posX,int posY, char letra) {
 
 
 }
-Animais::Animais(std::string an , int x, int y) {
-    std::string var[7] =  {"o","c"};
-    Reserva r;
-    if(var[2]==an){
-        Animais d(30,100,35,6,9,'O');
-        r.addanimal(x,y,'O',d);
-    }
 
-}
-int Animais::getID() const {
+int Animais::getID()  {
     return id;
 }
 
 
-int Animais::getPosX() const {
+int Animais::getPosX()  {
     return posx;
 }
 
-int Animais::getPosY() const {
+int Animais::getPosY()  {
     return posy;
 }
 
-char Animais::getSigla() const {
+char Animais::getSigla()  {
     return sig;
 }
 
-void Animais::Animaiscreatal(char animal) {
 
 
-}
-
-
-
-/*void Animais::setPos( int x, int y)  {
-    posx=x;
-    posy=y;
-}*/
