f758d117de119a5711597dca34fe9954ea27a605
RodrigoD19 <121464309+RodrigoD19@users.noreply.github.com>
2022-12-30 12:41:29 +0000

    Update Reserva.h

--- Trabalhocomerro/Reserva.h
+++ Trabalhocomerro/Reserva.h
@@ -7,22 +7,20 @@
 #include "Animais.h"
 #include "Terminal.h"
 class Reserva {
-    char ***c;
     int nlinhas, ncol;
     int nanimais, nalimentos;
     std::vector<Alimentos*>al;
     std::vector<Animais*>an;
 public:
-    Reserva();
+   Reserva();
     ~Reserva();
     int getnumlinhas();
     int getnumcolunas();
-    char printreserva(term::Window& tl,int x,int y);
-    int addanimal(int x,int y,char sig,Animais& a);
+    int addanimal(int x,int y,char sig, Window &t);
+    int addalimento(int x,int y,char sig, Window &t);
     int killanimal();
     char printreserva2(term::Window& tl,int x,int y);
     int askpositions(int **a,char *b);
-    char search(int x,int y);
 
 };
 
